ee9f65379312859045416b7a8c3a2ac0955e26d5
Mário Lucas <maryosk8bkt@gmail.com>
2017-10-06 18:04:08 -0300

    resume

---
+++ index.js
@@ -0,0 +1,8 @@
+//function prompt show input for received user data EX:
+
+var name = prompt("What's your name");
+console.log(name);
+
+//alter dom with user prompt data user
+
+document.getElementById("alterHere").innerHTML = prompt("Write text for div alterHere");
