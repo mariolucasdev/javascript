f1a8840538e41c893ac2b4ec6897561adaaae8ca
Mário Lucas <maryosk8bkt@gmail.com>
2017-10-08 20:45:43 -0300

    fix bug input

--- manipulandoLista/index.js
+++ manipulandoLista/index.js
@@ -1,10 +1,15 @@
+function resetInput(){
+    document.getElementById("item").value = "";
+}
+
 function addItem(){
     var item = document.getElementById("item").value;
     var todoList = document.getElementById("todoList").innerHTML;
 
-    todoList = todoList + "<li>"+ item +"</li>";
+    todoList = todoList + "<li>"+ item +" - <button class='removeItem'>excluir</button></li>";
     document.getElementById("todoList").innerHTML = todoList;
 
+    resetInput();
 }
 
 function updateitem(){
